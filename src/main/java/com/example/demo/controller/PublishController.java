88e44b05e30be52875544453e63a8bcb3c7fece5
wudabao <1042364042@qq.com>
2020-06-11 08:17:01 +0800

    1.表单页面

---
+++ src/main/java/com/example/demo/controller/PublishController.java
@@ -0,0 +1,96 @@
+package com.example.demo.controller;
+
+import com.example.demo.mapper.QuestionMapper;
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.Question;
+import com.example.demo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * Created by codedrinker on 2019/5/2.
+ */
+@Controller
+public class PublishController {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/publish")
+    public String publish() {
+        return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam(value = "tag", required = false) String tag,
+            @RequestParam(value = "id", required = false) Long id,
+            HttpServletRequest request,
+            Model model) {
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
+
+        if (StringUtils.isBlank(title)) {
+            model.addAttribute("error", "标题不能为空");
+            return "publish";
+        }
+        if (StringUtils.isBlank(description)) {
+            model.addAttribute("error", "问题补充不能为空");
+            return "publish";
+        }
+        if (StringUtils.isBlank(tag)) {
+            model.addAttribute("error", "标签不能为空");
+            return "publish";
+        }
+
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                   user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+
+                }
+            }
+        }
+
+        //  User user = (User) request.getSession().getAttribute("user");
+
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
+            return "publish";
+        }
+
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setId(id);
+        questionMapper.create(question);
+        return "redirect:/";
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/mapper/QuestionMapper.java
@@ -0,0 +1,15 @@
+package com.example.demo.mapper;
+
+
+import com.example.demo.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag)values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag}) ")
+    default void create(Question question){
+
+    }
+}
